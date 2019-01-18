b9baec42dbd3460e384865159e0217f3b2c54d9b
nimomaina <nimomaina.nm@gmail.com>
2019-01-18 17:02:07 +0300

    Add github API KEY to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Github_API_KEY: '28cece694b07fcab872674026867b5c24c798c8d',
+  Gitusers_API_URL:'https://api.github.com/users/' + username + '?access_token=' + Github_API_KEY,
+  Gitrepositories:'https://api.github.com/repositories/' + repo + '?access_token=' + Github_API_KEY
+'
 };
 
 /*

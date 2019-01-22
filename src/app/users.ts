1dd1e547d9516a897d8afe88b54388d39ec92d1e
nimomaina <nimomaina.nm@gmail.com>
2019-01-22 14:27:06 +0300

    Add environment to git ignore

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,6 +1,6 @@
 export class Users {
-  constructor(public username: string, public image: string, public numFollowers: string,
-     public numFollowing: string,
+  constructor(public username: string, public avatar: string, public followers: string,
+     public following: string,
       public repo: string, public names: string, public email: string,
-       public reposite: string, public time: Date) {}
+       public repo_url: string, public createdAt: Date) {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+production:false,
   apiUrl: '28cece694b07fcab872674026867b5c24c798c8d',
   // Gitusers_API_URL:'https://api.github.com/users/nimomaina?access_token=' + '28cece694b07fcab872674026867b5c24c798c8d',
   // Gitrepositories:'https://api.github.com/repositories/' + repo + '?access_token=' + Github_API_KEY
